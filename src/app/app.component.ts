0674410f197b31202d93993cc6dba1bb34e7013d
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-07 01:33:09 +0300

    Debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalRegisterComponent } from './components/modals/modal-register/modal-register.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'todoapp-client';
+
+  constructor(private modalService: NgbModal) {}
+
+  openModal(){
+    this.modalService.open(ModalRegisterComponent);
+  }
 }
