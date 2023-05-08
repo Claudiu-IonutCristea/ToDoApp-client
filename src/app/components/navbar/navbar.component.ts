6998629f338049fea7d348a1dfe488e6330defc7
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-08 17:08:44 +0300

    Add Navbar Component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalLoginComponent } from '../modals/modal-login/modal-login.component';
+import { ModalRegisterComponent } from '../modals/modal-register/modal-register.component';
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(private modalService: NgbModal) {}
+
+  openRegisterModal(){
+    this.modalService.open(ModalRegisterComponent);
+  }
+
+  openLoginModal(){
+    this.modalService.open(ModalLoginComponent);
+  }
 }
