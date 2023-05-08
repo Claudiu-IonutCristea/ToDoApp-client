82937ac5abc5db65228b35179eaf3091881746ff
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-08 17:09:10 +0300

    Remove Open Modal Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ModalRegisterComponent } from './components/modals/modal-register/modal-register.component';
-import { ModalLoginComponent } from './components/modals/modal-login/modal-login.component';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +7,5 @@ import { ModalLoginComponent } from './components/modals/modal-login/modal-login
 export class AppComponent {
   title = 'todoapp-client';
 
-  constructor(private modalService: NgbModal) {}
-
-  openModal(){
-    this.modalService.open(ModalLoginComponent);
-  }
+  
 }
