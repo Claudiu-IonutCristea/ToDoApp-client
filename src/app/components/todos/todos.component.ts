6715fa8062a1f5917d6f32164df96853d9b2559f
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-09 14:58:51 +0300

    Add ToDo CRUD functions

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ToDo, IToDoItem } from 'src/app/Classes/ToDo';
 import { TODOs } from 'src/app/mock-todos';
-import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalConfirmDeleteTodoComponent } from '../modals/modal-confirm-delete-todo/modal-confirm-delete-todo.component';
 
 @Component({
   selector: 'app-todos',
@@ -11,6 +12,9 @@ import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 export class TodosComponent{
   todos: ToDo[] = TODOs;
 
+  constructor(
+    private modalService: NgbModal
+    ) {}
 
   addNewItem(todo: ToDo, text: string){
     if(text){
@@ -22,7 +26,27 @@ export class TodosComponent{
     
   }
 
+  addTodo(title: string){
+    if(title){
+      this.todos.unshift(new ToDo(title));
+    }
+  }
+
   updateCheck(item: IToDoItem){
     item.completed = !item.completed;
+  }
+
+  deleteItem(todo: ToDo, item: IToDoItem){
+    const i: number = todo.todoItems.indexOf(item);
+    todo.todoItems.splice(i, 1);
+  }
+
+  deleteTodo(todo: ToDo){
+    const modalRef = this.modalService.open(ModalConfirmDeleteTodoComponent);
+    modalRef.componentInstance.todoName = todo.title;
+    modalRef.componentInstance.delete.subscribe(() => {
+      const i: number = this.todos.indexOf(todo, 0);
+      this.todos.splice(i, 1);
+    })
   }
 }
