c19e98fa9eace9becb3584d3c5e3c0d37156865c
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-10 20:43:10 +0300

    Local Storage

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,20 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ToDo, IToDoItem } from 'src/app/Classes/ToDo';
 import { TODOs } from 'src/app/mock-todos';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalConfirmDeleteTodoComponent } from '../modals/modal-confirm-delete-todo/modal-confirm-delete-todo.component';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html'
 })
 
-export class TodosComponent{
-  todos: ToDo[] = TODOs;
+export class TodosComponent implements OnInit{
+  @Input() useLocalStorage = false;
+
+  TODOSLOCAL = 'todoslocal';
+
+  todos = new Array<ToDo>();
+
+  // todos: ToDo[] = TODOs;
+  // todosLocal: ToDo[] = this.setupLocal();
+
+  ngOnInit(): void {
+    if(this.useLocalStorage){
+      this.todos = this.setupLocal();
+    }
+  }
 
   constructor(
-    private modalService: NgbModal
-    ) {}
+    private modalService: NgbModal,
+    private storageService: StorageService,
+  ) {}
 
   addNewItem(todo: ToDo, text: string){
     if(text){
@@ -23,13 +38,13 @@ export class TodosComponent{
        completed: false
       })
     }
-    
   }
 
   addTodo(title: string){
     if(title){
-      this.todos.unshift(new ToDo(title));
+      this.todos?.unshift(new ToDo(title));
     }
+    console.log(this.todos);
   }
 
   updateCheck(item: IToDoItem){
@@ -37,7 +52,7 @@ export class TodosComponent{
   }
 
   deleteItem(todo: ToDo, item: IToDoItem){
-    const i: number = todo.todoItems.indexOf(item);
+    const i = todo.todoItems.indexOf(item);
     todo.todoItems.splice(i, 1);
   }
 
@@ -45,8 +60,54 @@ export class TodosComponent{
     const modalRef = this.modalService.open(ModalConfirmDeleteTodoComponent);
     modalRef.componentInstance.todoName = todo.title;
     modalRef.componentInstance.delete.subscribe(() => {
-      const i: number = this.todos.indexOf(todo, 0);
+      const i = this.todos?.indexOf(todo, 0);
       this.todos.splice(i, 1);
     })
+  }
+
+  setupLocal(): ToDo[]{
+    const todosLocalJson = localStorage.getItem(this.TODOSLOCAL);
+    let todosLocalParsed;
+    let todosLocal = new Array<ToDo>();
+    if(todosLocalJson == null){
+      //no local todos
+      const tutorialTodo = this.tutorialTodo();
+      localStorage.setItem(this.TODOSLOCAL, JSON.stringify(Array<ToDo>(tutorialTodo)));
+      todosLocal.push(this.tutorialTodo());
+      console.log(this.tutorialTodo());
+      return todosLocal;
+    }
+    else{
+      todosLocalParsed = JSON.parse(todosLocalJson);
+    }
+
+    for(let todo of todosLocalParsed){
+      todosLocal.push(new ToDo(todo.title, todo.todoItems, todo.id));
+    }
+
+    return todosLocal;
+
+  }
+
+  tutorialTodo(): ToDo{
+    return new ToDo(
+      "Tutorial",
+      new Array<IToDoItem>(
+        {
+          text: "Test",
+          completed: false
+        },
+        {
+          text: "This is a test",
+          completed: true
+        },
+        {
+          text: "Test",
+          completed: false
+        },
+        
+      ),
+      "tutorialTodo"
+    );
   }
 }
