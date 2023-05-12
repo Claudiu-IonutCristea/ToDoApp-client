18a4a542c63dfb61aec8d48150ca2a4713c24acb
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-12 21:22:13 +0300

    local storage save

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,11 +1,15 @@
 import { Injectable, Inject, Optional } from '@angular/core';
+import { ToDo } from '../Classes/ToDo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
+  public TODOSLOCAL = 'todoslocal';
 
-  constructor(
-    
-  ) { }
+
+  saveLocal(todos: ToDo[]){
+    localStorage.setItem('todoslocal', JSON.stringify(todos));
+  }
+
 }
