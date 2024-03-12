0542fea2fbff2ff01b59675f35fffb75f8bfa6a1
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2024-03-12 06:08:55 +0200

    commit

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalLoginComponent } from '../modals/modal-login/modal-login.component';
 import { ModalRegisterComponent } from '../modals/modal-register/modal-register.component';
+
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
-  constructor(private modalService: NgbModal) {}
+  constructor(
+    private modalService: NgbModal,
+    private deviceService: DeviceDetectorService
+    ) {}
+
+  ngOnInit(): void {
+    console.log(this.deviceService.getDeviceInfo());
+    //this.deviceService.
+  }
 
   openRegisterModal(){
     this.modalService.open(ModalRegisterComponent);
